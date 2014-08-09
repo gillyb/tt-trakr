18e92848af3739316fc277fee17287c8e63af6e3
gillyb <gillyb@gmail.com>
2014-08-09 20:09:16 +0300

    styled the settings window

---
+++ js/settings.js
@@ -0,0 +1,14 @@
+
+var settingsService = angular.module('SettingsService', []);
+
+settingsService.factory('Settings', ['$http', '$q', function($http, $q) {
+
+	var fs = require('fs');
+
+	var settings = {
+		
+	};
+
+	return settings;
+
+}]);
